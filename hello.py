af83dbb161eb2730e289753d40958e4f09b5fa62
dsa1159 <63217408+dsa1159@users.noreply.github.com>
2020-04-08 10:09:07 +0900

    GitHub for git pull

--- hello.py
+++ hello.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // For command line git tutorial
 // First: GitHub modification
 // Second: Local repository modification
@@ -7,3 +6,4 @@ print("Hello world")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
